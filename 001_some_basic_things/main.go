d7f79dc992c2d0609fa5eda5c5d76a53d0d1ddab
alibaghernia <alibaghernia0@gmail.com>
2023-04-22 10:33:36 +0330

    [1]: Variable

--- 001_some_basic_things/main.go
+++ 001_some_basic_things/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// Comment:
 	//	this is a single line comment
@@ -8,4 +10,15 @@ func main() {
 		multi line comment
 		second line of multi line comment
 	*/
+
+	// ---------------------------------------------
+	// Variable
+	var name string = "Ali"
+	name2 := "Kourosh"
+	fmt.Println("name is:", name, "name2 is:", name2)
+
+	var number int
+	number = 5 + 1
+	fmt.Println("number is:", number)
+
 }
